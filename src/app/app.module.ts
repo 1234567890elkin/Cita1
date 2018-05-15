8769e7247fc98f983c03cfa949868de234ae8574
1234567890elkin <arquiteko.sas@gmail.com>
2018-05-15 18:19:41 -0500

    Creacion del arbol & Icono

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { ArbolComponent } from './components/arbol/arbol.component';
+import { PruebasComponent } from './components/pruebas/pruebas.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    ArbolComponent,
+    PruebasComponent
   ],
   imports: [
     BrowserModule,
